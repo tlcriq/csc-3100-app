5fadcc593b95de5640f69f07fc78bb54f9b0112b
tlcriq <tatecriqui@gmail.com>
2026-09-09 01:24:16 -0700

    Step 4 done

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -30,17 +30,29 @@ const users = {
   ],
 };
 
+const findUserByName = (name) => {
+  return users["users_list"].filter((user) => user["name"] === name);
+};
+
 const app = express();
 const port = 8000;
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Goodbye World!");
+  res.send("Hello World!");
 });
 
+
 app.get("/users", (req, res) => {
-  res.send(users);
+  const name = req.query.name;
+  if (name != undefined) {
+    let result = findUserByName(name);
+    result = { users_list: result };
+    res.send(result);
+  } else {
+    res.send(users);
+  }
 });
 
 app.listen(port, () => {
