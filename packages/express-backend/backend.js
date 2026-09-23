835f16c5e896b410583de7bf4c6437b3f061bea4
tlcriq <tatecriqui@gmail.com>
2026-09-23 12:30:33 -0700

    Step 4 complete

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,57 +1,9 @@
 import express from "express";
 import cors from "cors";
+import user from "user.js";
+import services from "user-service.js";
 
-const users = {
-  users_list: [
-    {
-      id: "789789",
-      name: "Charlie",
-      job: "Janitor",
-    },
-    {
-      id: "321123",
-      name: "Mac",
-      job: "Bouncer",
-    },
-    {
-      id: "555222",
-      name: "Mac",
-      job: "Professor",
-    },
-    {
-      id: "816999",
-      name: "Dee",
-      job: "Aspring actress",
-    },
-    {
-      id: "904555",
-      name: "Dennis",
-      job: "Bartender",
-    },
-  ],
-};
-
-const findUserByName = (name) => {
-  return users["users_list"].filter((user) => user["name"] === name);
-};
-
-const findUserById = (id) =>
-  users["users_list"].find((user) => user["id"] === id);
-
-const findUserByNameAndJob = (name, job) => {
-  return users["users_list"].filter((user) => user["name"] === name && user["job"] === job );
-};
-
-const addUser = (user) => {
-    users["users_list"].push(user);
-    return user;
-}
-const removeUser = (id) => {
-    const index = users["users_list"].findIndex( (element) => id === element.id );
-    users["users_list"].splice(index, 1);
-    return index;
-}
-const genID = () => {
+/*const genID = () => {
   var newID = 0;
   while(true) {
     newID = parseInt(1_000_000 * Math.random()).toString()
@@ -59,8 +11,7 @@ const genID = () => {
       break;
   }
   return newID;
-}
-
+}*/
 
 const app = express();
 const port = 8000;
@@ -76,49 +27,74 @@ app.get("/users", (req, res) => {
   const name = req.query.name;
   const job = req.query.job;
   if (name != undefined && job != undefined) {
-    let result = findUserByNameAndJob(name, job);
-    result = { users_list: result };
-    res.send(result);
+    services.findUserByNameAndJob(name, job).then((response) => {
+        res.send(response);
+      }).catch((error) => {
+        console.log(error);
+      });
   }
   else if (name != undefined) {
-    let result = findUserByName(name);
-    result = { users_list: result };
-    res.send(result);
+    services.getUsers(name, undefined).then((response) => {
+        res.send(response);
+      }).catch((error) => {
+        console.log(error);
+      });
   } else {
-    res.send(users);
+    services.getUsers().then((response) => {
+        res.send(response);
+      }).catch((error) => {
+        console.log(error);
+      });
   }
 });
 
 app.get("/users/:id", (req, res) => {
   const id = req.params["id"]; //or req.params.id
-  let result = findUserById(id);
-  if (result === undefined) {
-    res.status(404).send("Resource not found.");
-  } else {
-    res.send(result);
-  }
+  services.findUserById(id).then((response) => {
+    if(response.length == 0) {
+      res.status(404).send("Resource not found.");
+    }
+    else {
+      res.send(response);
+    }
+    }).catch((error) => {
+      console.log(error);
+    });
 });
 
 app.get("/users/", (req, res) => {
   const id = req.params["id"]; //or req.params.id
-  let result = findUserById(id);
-  if (result === undefined) {
-    res.status(404).send("Resource not found.");
-  } else {
-    res.send(result);
-  }
+
+  services.findUserById(id).then((response) => {
+    if(response.length == 0) {
+      res.status(404).send("Resource not found.");
+    }
+    else {
+      res.send(response);
+    }
+    }).catch((error) => {
+      console.log(error);
+    });
 });
 
 app.post("/users", (req, res) => {
     const userToAdd = req.body;
-    userToAdd.id = genID();
-    addUser(userToAdd);
-    res.status(201).send(userToAdd);
+    //userToAdd.id = genID();
+    services.addUser(userToAdd).then(() => {
+      res.status(201).send(userToAdd);
+    }
+    );
 });
 
 app.delete("/users/:id", (req, res) => {
   const id = req.params["id"];
-  let result = removeUser(id);
+  let result = services.removeUser((response) => {
+    if(response == null) {
+      res.status(404).send("No user matches this id.");
+    } else {
+      res.status(204).send();
+    }
+  });
   if (result == -1) {
     res.status(404).send("No user matches this id.");
   } else {
